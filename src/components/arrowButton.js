cae9e0189b6c3cbf036957ec9adfee555a132cdc
Duncan Leo <hello@duncanleo.me>
2020-01-19 00:25:35 +0800

    chore: arrow button improvements (PR: #24)
    * chore: remove unused svg file
    
    * refactor: ArrowButton

--- src/components/arrowButton.js
+++ src/components/arrowButton.js
@@ -3,52 +3,67 @@ import { Link } from "gatsby";
 import PropTypes from "prop-types";
 import styled from "styled-components";
 
-const ArrowButton = ({ to }) => {
-  const Button = styled.button`
-    display: flex;
-    align-items: center;
-    outline: 1px solid black;
-    text-decoration: none;
-    font-weight: 700;
-    background: none;
-    border: 1px solid ${props => props.theme.black};
-    padding: 18px 30px;
-  `;
+const IconWrapper = styled.div`
+  display: inline-block;
+  padding: 5px 0px 0px 10px;
+  transition: transform 40ms ease-in-out;
+`;
 
+const Wrapper = styled(Link)`
+  display: flex;
+  align-items: center;
+  outline: 1px solid black;
+  text-decoration: none;
+  font-weight: 700;
+  background-color: transparent;
+  border: 1px solid ${props => props.theme.black};
+  padding: 18px 30px;
+  transition: background-color 20ms;
+
+  &:hover {
+    cursor: pointer;
+    color: ${props => props.theme.white};
+    background-color: ${props => props.theme.orange};
+
+    ${IconWrapper} {
+      transform: translateX(10%);
+    }
+
+    svg {
+      path {
+        stroke: ${props => props.theme.white};
+      }
+    }
+  }
+`;
+
+const ArrowButton = ({ to }) => {
   return (
-    <Link
-      to={to}
-      style={{
-        textDecoration: "none",
-        color: "black",
-      }}
-    >
-      <Button>
-        Take the Test
-        <div style={{ display: "inline-block", padding: "5px 0px 0px 10px" }}>
-          <svg
-            width="41"
-            height="22"
-            viewBox="0 0 41 22"
-            fill="none"
-            xmlns="http://www.w3.org/2000/svg"
-          >
-            <path
-              d="M1 10.7914L40 10.7914"
-              stroke="black"
-              strokeLinecap="round"
-              strokeLinejoin="round"
-            />
-            <path
-              d="M30 1L40 10.7915L30 20.5829"
-              stroke="black"
-              strokeLinecap="round"
-              strokeLinejoin="round"
-            />
-          </svg>
-        </div>
-      </Button>
-    </Link>
+    <Wrapper to={to}>
+      Take the Test
+      <IconWrapper>
+        <svg
+          width="41"
+          height="22"
+          viewBox="0 0 41 22"
+          fill="none"
+          xmlns="http://www.w3.org/2000/svg"
+        >
+          <path
+            d="M1 10.7914L40 10.7914"
+            stroke="black"
+            strokeLinecap="round"
+            strokeLinejoin="round"
+          />
+          <path
+            d="M30 1L40 10.7915L30 20.5829"
+            stroke="black"
+            strokeLinecap="round"
+            strokeLinejoin="round"
+          />
+        </svg>
+      </IconWrapper>
+    </Wrapper>
   );
 };
 
