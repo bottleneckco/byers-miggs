d90f19e4544c48c09bbce104cdaa95e343c7b60e
Duncan Leo <hello@duncanleo.me>
2020-01-19 02:11:17 +0800

    chore: update FAQ copy

--- src/components/faq.js
+++ src/components/faq.js
@@ -72,48 +72,23 @@ function FAQ({ id }) {
         Questions
       </Title>
       <FAQWrapper>
-        <FaqQuestion question="Lorem ipsum dolor sit amet, consectetur adipiscing elit.">
-          Aenean nec magna luctus, porttitor neque at, aliquet nulla. Mauris
-          sagittis venenatis felis ultricies ultrices. Pellentesque varius
-          scelerisque sapien id efficitur. Quisque in augue at dolor egestas
-          auctor. Etiam in justo et lectus efficitur ornare et sit amet risus.
-          Sed ut cursus sapien, at laoreet urna. Cras neque enim, fringilla sed
-          ultrices a, vestibulum id massa.
+        <FaqQuestion question="Is this accurate?">
+          Definitely. We wouldn’t waste time making this site beautiful if it
+          wasn’t.
         </FaqQuestion>
-        <FaqQuestion question="Quisque commodo pellentesque suscipit. Etiam blandit a justo id sollicitudin.">
-          Donec mattis iaculis felis quis lacinia. Donec auctor tempor eros et
-          blandit. Duis a egestas tortor. Sed tincidunt maximus maximus.
-          Maecenas malesuada aliquet aliquet. Nulla ut quam ipsum. Morbi
-          pellentesque ex sed pretium auctor.
+        <FaqQuestion question="Should I use this site?">
+          You already are. Might as well get through it.
         </FaqQuestion>
-        <FaqQuestion question="Mauris hendrerit finibus nisi ut porttitor. Nulla pulvinar neque id cursus efficitur.">
-          Pellentesque efficitur dui vel tempus efficitur. Sed ipsum ligula,
-          pretium quis dui a, aliquet consequat mauris. Vestibulum eu lectus
-          condimentum, blandit libero lobortis, porttitor lectus. Quisque in
-          efficitur massa. Praesent risus urna, faucibus a pellentesque id,
-          iaculis at augue. Fusce cursus placerat urna, vitae iaculis nisi
-          vestibulum sit amet. Praesent eget faucibus risus, id interdum lacus.
-          In pretium hendrerit sodales. Nulla facilisi. Morbi viverra eros non
-          felis aliquet tempus.
+        <FaqQuestion question="Is this site GDPR-compliant?">
+          Byers-Miggs is compliant with the 2018 European Union GDPR
+          regulations.
         </FaqQuestion>
-        <FaqQuestion question="Fusce nec efficitur nulla. Aliquam aliquam odio nec quam pulvinar pharetra.">
-          Ut quis euismod turpis. Morbi quam urna, ultrices ut orci vitae,
-          posuere auctor dolor. Duis sit amet euismod sem. Aliquam nibh nisl,
-          laoreet eget dui ut, tempus tempor lacus. Fusce interdum tristique
-          elit, ut imperdiet est fermentum a. Maecenas maximus neque dui, at
-          dictum sapien tempus et. Fusce semper consectetur mi, in ornare nisl
-          tincidunt nec. Suspendisse ut dictum lorem. Maecenas leo felis,
-          molestie non nulla dictum, tristique tristique augue. Pellentesque
-          lacinia at purus vel volutpat. Donec pretium pharetra erat non
-          tincidunt. Ut non sem lorem. Donec mi dolor, tempus vitae venenatis
-          quis, faucibus ut lacus.
+        <FaqQuestion question="Is my data secure?">
+          Your data is stored in a secure facility with the latest AES-6144
+          encryption.
         </FaqQuestion>
-        <FaqQuestion question="Phasellus quis pellentesque sapien.">
-          Donec vitae tristique risus, ut accumsan sem. Nulla tempus nisi
-          iaculis lectus scelerisque, ac egestas neque aliquam. Maecenas porta
-          lacus at odio luctus fermentum. Integer scelerisque nisi eget cursus
-          lobortis. Nullam nec tortor tristique, placerat ante eu, commodo elit.
-          Pellentesque blandit ultricies velit eu rutrum.
+        <FaqQuestion question="What is your return policy?">
+          We do not offer returns.
         </FaqQuestion>
       </FAQWrapper>
     </Wrapper>
