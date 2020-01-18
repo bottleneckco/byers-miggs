c667fe45a33312ef10b9f05b1194aacb9fd084e5
Duncan Leo <hello@duncanleo.me>
2020-01-18 14:47:41 +0800

    chore: wip

--- src/pages/face.js
+++ src/pages/face.js
@@ -1,11 +1,28 @@
 import React, { useEffect, useRef } from "react";
 import styled from "styled-components";
+import * as faceApi from "face-api.js";
+import { useState } from "react";
 
-const Video = styled.video``;
+const Wrapper = styled.div`
+  display: grid;
+`;
+
+const Video = styled.video`
+  grid-area: 1/1;
+`;
+
+const Canvas = styled.canvas`
+  grid-area: 1/1;
+`;
 
 function FacePage() {
+  const [faces, setFaces] = useState([]);
+  const [isVideoReady, setIsVideoReady] = useState(false);
+
+  const canvasRef = useRef(null);
   const videoRef = useRef(null);
 
+  // Setup HTML5 Camera
   useEffect(() => {
     async function setupCamera() {
       const stream = await navigator.mediaDevices.getUserMedia({
@@ -14,14 +31,57 @@ function FacePage() {
 
       videoRef.current.srcObject = stream;
       videoRef.current.play();
+      setIsVideoReady(true);
     }
     setupCamera();
   }, []);
+
+  // Load models
+  useEffect(() => {
+    async function loadFaceApiModels() {
+      await faceApi.nets.tinyFaceDetector.loadFromUri("./models");
+      console.log("Loaded");
+    }
+    loadFaceApiModels();
+  }, []);
+
+  // Draw faces
+  useEffect(() => {
+    const displaySize = {
+      width: videoRef.current.videoWidth,
+      height: videoRef.current.videoHeight,
+    };
+    faceApi.matchDimensions(canvasRef.current, displaySize);
+    faceApi.draw.drawDetections(canvasRef.current, faces);
+  }, [faces]);
+
+  async function detectFaces() {
+    const facesArray = await faceApi.detectAllFaces(
+      videoRef.current,
+      new faceApi.TinyFaceDetectorOptions()
+    );
+    setFaces(facesArray);
+  }
+
+  // Detect faces
+  useEffect(() => {
+    function callback() {
+      if (faces && faces.length > 0) {
+        detectFaces();
+      }
+      window.requestAnimationFrame(callback);
+    }
+    const handle = window.requestAnimationFrame(callback);
+    return () => window.cancelAnimationFrame(handle);
+  }, [isVideoReady]);
+
   return (
-    <div>
+    <Wrapper>
       <h1>THE FACE PAGE</h1>
       <Video ref={videoRef} />
-    </div>
+      <Canvas ref={canvasRef} />
+      <button onClick={detectFaces}>Fuck</button>
+    </Wrapper>
   );
 }
 
