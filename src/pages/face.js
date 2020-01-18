2fbbc02c889f0bf94da9e22a6a93b756382055a6
Duncan Leo <hello@duncanleo.me>
2020-01-18 17:11:01 +0800

    feat: initial face page (#1)
    * chore: add face page
    
    * chore: add 'face-api.js' package
    
    * chore: wip
    
    * fix: detection actually works
    
    * chore: detect single face only
    
    * chore: add customisation options for box
    
    * refactor: move Camera logic into its own component
    
    * chore: wrap FacePage in Gatsby layout
    
    * chore: add facial expressions detection

---
+++ src/pages/face.js
@@ -0,0 +1,138 @@
+import React, { useEffect, useRef } from "react";
+import styled from "styled-components";
+import * as faceApi from "face-api.js";
+import { useState } from "react";
+import Layout from "../components/layout";
+import SEO from "../components/seo";
+
+const BOX_LINE_COLOR = "darkcyan";
+const BOX_LINE_WIDTH = 2;
+
+const Wrapper = styled.div``;
+
+const CameraViewWrapper = styled.div`
+  display: grid;
+`;
+
+const Video = styled.video`
+  grid-area: 1/1;
+`;
+
+const Canvas = styled.canvas`
+  grid-area: 1/1;
+`;
+
+function getMostLikelyExpression(obj) {
+  return Object.keys(obj).reduce((a, b) => (obj[a] > obj[b] ? a : b));
+}
+
+function useCameraViewState() {
+  const [face, setFace] = useState(null);
+  const [isVideoReady, setIsVideoReady] = useState(false);
+
+  const canvasRef = useRef(null);
+  const videoRef = useRef(null);
+
+  // Setup HTML5 Camera
+  useEffect(() => {
+    async function setupCamera() {
+      const stream = await navigator.mediaDevices.getUserMedia({
+        video: true,
+      });
+
+      videoRef.current.srcObject = stream;
+      videoRef.current.play();
+      setIsVideoReady(true);
+    }
+    setupCamera();
+  }, []);
+
+  // Load models
+  useEffect(() => {
+    async function loadFaceApiModels() {
+      await faceApi.nets.tinyFaceDetector.loadFromUri("./models");
+      await faceApi.nets.faceExpressionNet.loadFromUri("./models");
+      console.log("Loaded");
+    }
+    loadFaceApiModels();
+  }, []);
+
+  // Draw face
+  useEffect(() => {
+    const displaySize = {
+      width: videoRef.current.videoWidth,
+      height: videoRef.current.videoHeight,
+    };
+    faceApi.matchDimensions(canvasRef.current, displaySize);
+    if (face) {
+      const { detection, expressions } = face;
+      new faceApi.draw.DrawBox(detection.box, {
+        boxColor: BOX_LINE_COLOR,
+        lineWidth: BOX_LINE_WIDTH,
+        label: `${detection.score.toFixed(2)}\n${getMostLikelyExpression(
+          expressions
+        ).toUpperCase()}`,
+      }).draw(canvasRef.current);
+    }
+  }, [face]);
+
+  async function detectFaces() {
+    const detectedFace = await faceApi
+      .detectSingleFace(
+        videoRef.current,
+        new faceApi.TinyFaceDetectorOptions({
+          inputSize: 128,
+          scoreThreshold: 0.3,
+        })
+      )
+      .withFaceExpressions();
+    setFace(detectedFace);
+  }
+
+  // Detect face
+  useEffect(() => {
+    if (!isVideoReady || !videoRef.current) {
+      return;
+    }
+    function callback() {
+      detectFaces();
+      window.requestAnimationFrame(callback);
+    }
+    const handle = window.requestAnimationFrame(callback);
+    return () => window.cancelAnimationFrame(handle);
+  }, [videoRef, isVideoReady]);
+
+  return {
+    face,
+    setFace,
+    isVideoReady,
+    setIsVideoReady,
+    canvasRef,
+    videoRef,
+  };
+}
+
+function CameraView() {
+  const cameraViewState = useCameraViewState();
+  return (
+    <CameraViewWrapper>
+      <Video ref={cameraViewState.videoRef} />
+      <Canvas ref={cameraViewState.canvasRef} />
+      <span>{cameraViewState.face ? "GOT PEOPLE" : "NO PEOPLE"}</span>
+    </CameraViewWrapper>
+  );
+}
+
+function FacePage() {
+  return (
+    <Layout>
+      <SEO title="Face" />
+      <Wrapper>
+        <h1>THE FACE PAGE</h1>
+        <CameraView />
+      </Wrapper>
+    </Layout>
+  );
+}
+
+export default FacePage;
